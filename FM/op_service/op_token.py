c04a556aa31cb7962f5d9cbcc415b7524eabd6a6
bashsiz <morteza.bashsiz@gmail.com>
2017-11-21 15:18:53 +0330

    update logger

--- FM/op_service/op_token.py
+++ FM/op_service/op_token.py
@@ -4,8 +4,8 @@ import op_logger as log
 
 def validate_token(token):
     _MysqlData=conf.mysql_data()
+    db = mariadb.connect(host=_MysqlData['host'],port=_MysqlData['port'],user=_MysqlData['user'],password=_MysqlData['pass'],database=_MysqlData['name'])
     try:
-        db = mariadb.connect(host=_MysqlData['host'],port=_MysqlData['port'],user=_MysqlData['user'],password=_MysqlData['pass'],database=_MysqlData['name'])
         cursor = db.cursor()
         _log_message = "TOKEN CONNECTED TO MYSQL host="+_MysqlData['host']+" , port="+_MysqlData['port']
         log.logger(_log_message)
@@ -16,15 +16,15 @@ def validate_token(token):
         cursor.execute("""SELECT EXISTS(SELECT token FROM tb_token WHERE token = %s )""", (token,))
         _token_exist = cursor.fetchone()
         if _token_exist[0] > 0:
-            result = "success"
+            result = 200
             _log_message = "TOKEN VALIDATED token="+token
         else:
-            result = "failed"
+            result = 554
             _log_message = "TOKEN INVALID token="+token
         log.logger(_log_message)
     except mariadb.Error , err:
         db.rollback()
-        result = "failed"
+        result = 554
         log.logger(str(err))
     db.close()
     return result
--- FM/op_service/op_url.py
+++ FM/op_service/op_url.py
@@ -1,6 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler
 from StringIO import StringIO
-from django.http import HttpResponse , HttpResponseNotFound , Http404
+from django.http import HttpResponse
 
 import op_mysql as db
 import op_token as token
@@ -22,21 +22,18 @@ def string_parser(string,delimiter):
     _SPLITS = re.split(delimiter,string)
     return _SPLITS
 
-ARR_INSERT=[1,1,1,1]
-ARR_S_D=[1,1]
-
 def url_pars(url):
     _SPLITS = re.split('/',url)
     if _SPLITS[1] == "insert" :
-        if token.validate_token(_SPLITS[2]) is "success" :
+        if token.validate_token(_SPLITS[2]) == 200 :
             return db.insert_peers(str(_SPLITS[3]),str(_SPLITS[4]),str(string_parser(_SPLITS[5],"'")[0]))
         else:  
-            return "failed"
+            return 554
     elif _SPLITS[1] == "delete" :
-        if token.validate_token(_SPLITS[2]) is "success" :
+        if token.validate_token(_SPLITS[2]) == 200 :
             return db.delete_peers(str(string_parser(_SPLITS[3],"'")[0]))
         else:  
-            return "failed"
+            return 554
 
 def insert(request):
         result=url_pars(str(request))
